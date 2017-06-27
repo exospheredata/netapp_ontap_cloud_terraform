fe277e7aebfbcd87aa33e4a3081e02a4f67cc671
Jeremy Goodrum <jeremy.b.goodrum@gmail.com>
2017-06-27 16:26:29 -0500

    * Release 1.5.0 with support for OnCommand Cloud Manager 3.3.0 ** aws_standalone ADD: Berksfile for cookbook resolution UPDATE: Files::occm-role-policy to include updated IAM requirements for OCCM 3.3.0 UPDATE: Script::bootstrap.sh to install the ChefDK client and use the Berksfile to package the cookbooks and dependencies. UPDATE: Templates::create_ontap.json.tpl to include variable to set the ontap_write_speed UPDATE: Main.tf to streamline resource idempotence and deployment UPDATE: Output.tf variable outputs UPDATE: templates.tf to include changes in setting the write_speed UPDATE: variables.tf formatting and defaults.
    ** aws_standalone_full
    ADD: Berksfile for cookbook resolution
    UPDATE: Files::occm-role-policy to include updated IAM requirements for OCCM 3.3.0
    UPDATE: Script::bootstrap.sh to install the ChefDK client and use the Berksfile to package the cookbooks and dependencies.
    UPDATE: Templates::create_ontap.json.tpl to include variable to set the ontap_write_speed
    UPDATE: Main.tf to streamline resource idempotence and deployment
    UPDATE: Output.tf variable outputs
    UPDATE: templates.tf to include changes in setting the write_speed
    UPDATE: variables.tf formatting and defaults.

--- aws_standalone/main.tf
+++ aws_standalone/main.tf
@@ -8,18 +8,18 @@ Create a new IAM Role that can be assigned to the OnCommand Cloud Manager
 EC2 instance and provide access controls to the connected AWS account.
 */
 resource "aws_iam_role" "occm_ec2_role" {
-  name = "occm_ec2_role"
-  description = "Grants access to services required by NetApp's OnCommand Cloud Manager"
-  assume_role_policy = "${file("files/occm-ec2-role.json")}"
+  name                = "occm_instance_profile_${replace(lower(var.owner_name)," ","_")}"
+  description         = "Grants access to services required by NetApp's OnCommand Cloud Manager"
+  assume_role_policy  = "${file("${path.module}/files/occm-ec2-role.json")}"
 }
 
 /*
 EC2 Instance Profile based on the provided role
 */
 resource "aws_iam_instance_profile" "occm_instance_profile" {
-  name = "occm_instance_profile"
-  role = "${aws_iam_role.occm_ec2_role.id}"
-  depends_on = [
+  name        = "occm_instance_profile_${replace(lower(var.owner_name)," ","_")}"
+  role        = "${aws_iam_role.occm_ec2_role.id}"
+  depends_on  = [
     "aws_iam_role.occm_ec2_role"
   ]
 }
@@ -27,14 +27,14 @@ resource "aws_iam_instance_profile" "occm_instance_profile" {
 /*
 Default policy document for privileges requireed by OnCommand Cloud Manager
 */
-resource "aws_iam_role_policy" "role_policy" {
-  name        = "occm_instance_role_policy"
+resource "aws_iam_role_policy" "occm_role_policy" {
+  name        = "occm_instance_profile_${replace(lower(var.owner_name)," ","_")}"
   role        = "${aws_iam_role.occm_ec2_role.id}"
-  depends_on = [
+  depends_on  = [
     "aws_iam_role.occm_ec2_role"
   ]
 
-  policy = "${file("files/occm-role-policy.json")}"
+  policy = "${file("${path.module}/files/occm-role-policy.json")}"
 }
 
 /*
@@ -42,9 +42,9 @@ New Security group granting Web access and SSH access to the OnCommand Cloud Man
 allow all outgoing traffic.
 */
 resource "aws_security_group" "occm_access" {
-  name        = "occm_access"
-  description = "Allow all inbound traffic on ports 80, 443, and 22."
-  vpc_id = "${data.aws_subnet.selected.vpc_id}"
+  name                = "occm_access_${replace(lower(var.owner_name)," ","_")}"
+  description         = "Allow all inbound traffic on ports 80, 443, and 22."
+  vpc_id              = "${data.aws_subnet.selected.vpc_id}"
 
   ingress {
     from_port   = 80
@@ -76,7 +76,8 @@ resource "aws_security_group" "occm_access" {
 
   tags {
     Name = "OnCommand Cloud Manager Access",
-    Created_By = "Terraform"
+    "Deployed Using" = "Terraform",
+    "Designed by" = "www.exospheredata.com"
   }
 
 }
@@ -86,25 +87,25 @@ Launch a new AWS Marketplace instance of Netapp's OnCommand Cloud Manager and as
 created EC2 Instance Profile.
 */
 resource "aws_instance" "OCCM" {
-  ami           							= "${lookup(var.occm_amis, var.region)}"
-  instance_type 							= "t2.medium"
-  subnet_id										= "${data.aws_subnet.selected.id}"
-  vpc_security_group_ids 			= ["${aws_security_group.occm_access.id}"]
-  key_name										= "${var.key_name}"
+  depends_on = [
+    "aws_iam_role_policy.occm_role_policy",
+    "aws_iam_role.occm_ec2_role",
+    "aws_security_group.occm_access"
+  ]
+  ami                         = "${lookup(var.occm_amis, var.region)}"
+  instance_type               = "t2.medium"
+  subnet_id                   = "${data.aws_subnet.selected.id}"
+  vpc_security_group_ids      = ["${aws_security_group.occm_access.id}"]
+  key_name                    = "${var.key_name}"
   associate_public_ip_address = "true"
-  iam_instance_profile = "${aws_iam_instance_profile.occm_instance_profile.id}"
+  iam_instance_profile        = "${aws_iam_instance_profile.occm_instance_profile.id}"
   tags {
     Name = "OnCommand Cloud Manager",
+    "Owned By" =  "${var.owner_name}",
     "Deployed Using" = "Terraform",
     "Provisioned Using" = "CHEF",
     "Designed by" = "www.exospheredata.com"
   }
-  depends_on = [
-    "aws_iam_role_policy.role_policy",
-    "aws_iam_role.occm_ec2_role",
-    "aws_iam_role_policy.role_policy",
-    "aws_security_group.occm_access"
-  ]
 
   /*
   Establish a connection to the OnCommand Cloud Manager system using
@@ -123,28 +124,116 @@ resource "aws_instance" "OCCM" {
   to SSH long before the service is online.
   */
   provisioner "remote-exec" {
-      inline = [
-        "mkdir -p /tmp"
-      ]
+    inline = [
+      "mkdir -p /tmp/"
+    ]
   }
   provisioner "file" {
-    source      = "./scripts/check_server_health.sh"
+    source      = "${path.module}/scripts/check_server_health.sh"
     destination = "/tmp/check_server_health.sh"
   }
   provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/check_server_health.sh",
-        "/tmp/check_server_health.sh",
-        "echo The Instance is ready to be provisioned"
-      ]
+    inline = [
+      "chmod +x /tmp/check_server_health.sh",
+      "/tmp/check_server_health.sh",
+      "echo The Instance is ready to be provisioned"
+    ]
+  }
+  /*
+  Establish a connection to the OnCommand Cloud Manager system using ssh and the private
+  key file provided.
+
+  The deployment process of an ONTAP Cloud system can take upwards of 25+ minutes depending
+  on the state of the AWS Region.  We are setting a 60 minute timeout on this connection
+  due to this potential length.
+  */
+  connection {
+    host = "${aws_instance.OCCM.0.public_ip}"
+    type = "ssh"
+    user = "ec2-user" # OCCM Default username in AWS EC2 instances
+    private_key = "${file("${var.key_file_path}")}"
+    timeout = "60m"
+  }
+  /*
+  ===========
+  These steps will only be run when the terraform destroy command is sent
+  ===========
+
+  The following will only run on a destroy of the environment and needs to be run
+  to properly destroy the ONTAP Cloud instance.
+  */
+  provisioner "remote-exec" {
+    when = "destroy"
+    inline = [
+      "rm -rf /tmp/chef",
+      "rm -rf /tmp/cookbooks.tar.gz",
+      "mkdir -p /tmp/chef/data_bags/occm",
+      "mkdir -p /tmp/ontap_cloud_cookbooks"
+    ]
+  }
+
+  /*
+  Create archive of the cookbooks and dependencies. Upon creation, upload the file to the server and then remove the local copy.
+
+  the path_root variable tells the system to look at the top level of the modules and not in the module path.
+  */
+  provisioner "file" {
+    when = "destroy"
+    source      = "${path.module}/files/Berksfile"
+    destination = "/tmp/ontap_cloud_cookbooks/Berksfile"
+  }
+
+  /*
+  Send all interpolated files to the remote server
+  */
+  provisioner "file" {
+    when = "destroy"
+    source      = "${path.module}/files/solo.rb"
+    destination = "/tmp/chef/solo.rb"
+  }
+  provisioner "file" {
+    when = "destroy"
+    content     = "${data.template_file.destroy_ontap.rendered}"
+    destination = "/tmp/chef/dna.json"
+  }
+  provisioner "file" {
+    when = "destroy"
+    source      = "${path.module}/scripts/bootstrap.sh"
+    destination = "/tmp/chef/bootstrap.sh"
+  }
+
+  /*
+  Setup the CHEF Data bags
+  */
+  provisioner "file" {
+    when = "destroy"
+    content     = "${data.template_file.admin_credentials.rendered}"
+    destination = "/tmp/chef/data_bags/occm/admin_credentials.json"
+  }
+
+  /*
+  Execute Bootstrap script to apply CHEF configuration and setup.
+  */
+  provisioner "remote-exec" {
+    when = "destroy"
+    inline = [
+      "chmod +x /tmp/chef/bootstrap.sh",
+      "sudo /tmp/chef/bootstrap.sh",
+    ]
   }
 }
 
+/*
+Deploy ONTAP Cloud system and configuration via CHEF
+*/
 resource "null_resource" "ontap_cloud" {
+  triggers {
+    occm_id = "${aws_instance.OCCM.id}",
+    converge = "${var.converge}"
+  }
   depends_on = [
-    "aws_iam_role_policy.role_policy",
+    "aws_iam_role_policy.occm_role_policy",
     "aws_iam_role.occm_ec2_role",
-    "aws_iam_role_policy.role_policy",
     "aws_instance.OCCM"
   ]
 
@@ -164,23 +253,39 @@ resource "null_resource" "ontap_cloud" {
     timeout = "60m"
   }
 
+  /*
+  ===========
+  These steps will only be run when the terraform apply command is sent
+  ===========
+
+  */
   provisioner "remote-exec" {
-      inline = [
-        "rm -rf /tmp/chef",
-        "mkdir -p /tmp/chef/data_bags/occm"
-      ]
+    inline = [
+      "rm -rf /tmp/chef",
+      "rm -rf /tmp/ontap_cloud_cookbooks",
+      "mkdir -p /tmp/chef/data_bags/occm",
+      "mkdir -p /tmp/ontap_cloud_cookbooks"
+    ]
+  }
+
+  /*
+  Send all interpolated files to the remote server
+  */
+  provisioner "file" {
+    source      = "${path.module}/files/Berksfile"
+    destination = "/tmp/ontap_cloud_cookbooks/Berksfile"
   }
   provisioner "file" {
-    source      = "./files/solo.rb"
+    source      = "${path.module}/files/solo.rb"
     destination = "/tmp/chef/solo.rb"
-  }
-  provisioner "file" {
-    source      = "./scripts/bootstrap.sh"
-    destination = "/tmp/chef/bootstrap.sh"
   }
   provisioner "file" {
     content     = "${data.template_file.create_ontap.rendered}"
     destination = "/tmp/chef/dna.json"
+  }
+  provisioner "file" {
+    source      = "${path.module}/scripts/bootstrap.sh"
+    destination = "/tmp/chef/bootstrap.sh"
   }
 
   /*
@@ -195,49 +300,10 @@ resource "null_resource" "ontap_cloud" {
     destination = "/tmp/chef/data_bags/occm/${var.ontap_name}.json"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/chef/bootstrap.sh",
-      "sudo /tmp/chef/bootstrap.sh",
-    ]
-  }
-
   /*
-  The following will only run on a destroy of the environment and needs to be run
-  to properly destroy the ONTAP Cloud instance.
+  Execute Bootstrap script to apply CHEF configuration and setup.
   */
   provisioner "remote-exec" {
-    when = "destroy"
-    inline = [
-      "rm -rf /tmp/chef",
-      "mkdir -p /tmp/chef/data_bags/occm"
-    ]
-  }
-  /*
-  Setup the CHEF Data bags
-  */
-  provisioner "file" {
-    when = "destroy"
-    content     = "${data.template_file.admin_credentials.rendered}"
-    destination = "/tmp/chef/data_bags/occm/admin_credentials.json"
-  }
-  provisioner "file" {
-    when = "destroy"
-    source      = "./files/solo.rb"
-    destination = "/tmp/chef/solo.rb"
-  }
-  provisioner "file" {
-    when = "destroy"
-    source      = "./scripts/bootstrap.sh"
-    destination = "/tmp/chef/bootstrap.sh"
-  }
-  provisioner "file" {
-    when = "destroy"
-    content     = "${data.template_file.destroy_ontap.rendered}"
-    destination = "/tmp/chef/dna.json"
-  }
-  provisioner "remote-exec" {
-    when = "destroy"
     inline = [
       "chmod +x /tmp/chef/bootstrap.sh",
       "sudo /tmp/chef/bootstrap.sh",
--- aws_standalone/templates.tf
+++ aws_standalone/templates.tf
@@ -5,24 +5,33 @@ data "aws_subnet" "selected" {
   id = "${var.subnet_id}"
 }
 
+/*
+Create the data_bag object to contain the OnCommand Cloud Manager credentials
+*/
 data "template_file" "admin_credentials" {
-  template = "${file("templates/data_bags/occm/admin_credentials.json.tpl")}"
+  template = "${file("${path.module}/templates/data_bags/occm/admin_credentials.json.tpl")}"
   vars {
     occm_email = "${var.occm_email}"
     occm_password = "${var.occm_password}"
   }
 }
 
+/*
+Create the data_bag object to contain the ONTAP Cloud credentials
+*/
 data "template_file" "ontap_credentials" {
-  template = "${file("templates/data_bags/occm/ontap_credentials.json.tpl")}"
+  template = "${file("${path.module}/templates/data_bags/occm/ontap_credentials.json.tpl")}"
   vars {
     ontap_name = "${var.ontap_name}"
     svm_password = "${var.ontap_password}"
   }
 }
 
+/*
+Create the JSON attributes file for use when creating ONTAP Cloud systems via CHEF
+*/
 data "template_file" "create_ontap" {
-  template = "${file("templates/create_ontap.json.tpl")}"
+  template = "${file("${path.module}/templates/create_ontap.json.tpl")}"
   vars {
     company_name = "${var.company_name}"
     ontap_name = "${var.ontap_name}"
@@ -32,11 +41,15 @@ data "template_file" "create_ontap" {
     vpc_id = "${data.aws_subnet.selected.vpc_id}"
     region = "${var.region}"
     subnet_id = "${data.aws_subnet.selected.id}"
+    write_speed = "${lower(var.ontap_write_speed)}"
   }
 }
 
+/*
+Create the JSON attributes file for use when deleting ONTAP Cloud systems via CHEF
+*/
 data "template_file" "destroy_ontap" {
-  template = "${file("templates/destroy_ontap.json.tpl")}"
+  template = "${file("${path.module}/templates/destroy_ontap.json.tpl")}"
   vars {
     ontap_name = "${var.ontap_name}"
   }
